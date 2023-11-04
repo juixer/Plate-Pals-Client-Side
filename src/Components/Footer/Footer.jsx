4f5e73dde24c3aa229b3b074f6c56e57908b2397
Ashfakul Islam <r4pido3@gmail.com>
2023-11-05 00:47:46 +0600

    footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,53 @@
-import { Footer as Foooter } from 'flowbite-react';
-import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
-
-const Foooter = () => {
-    return(
-        <div>
-             <p> HELLO I Am Footer </p>
+import { Footer } from "flowbite-react";
+import {
+  BsDribbble,
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsTwitter,
+} from "react-icons/bs";
+const FooTer = () => {
+  return (
+    <Footer container className="bg-gray-100">
+      <div className="w-full max-w-screen-xl mx-auto">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div>
+            <Footer.Brand
+              href="/"
+              src="https://i.ibb.co/Z69QK90/pngtree-fast-food-logo-png-image-5763171.png"
+              name="PlatePals"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="about" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Our Team</Footer.Link>
+                <Footer.Link href="#">Our Office</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
         </div>
-    )}
-export default Foooter;
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="All right reserved by PlatePals" year={2023} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+};
+export default FooTer;
--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -2,19 +2,23 @@ import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "../Components/Navbar/NavBar";
 import loading from "../../public/loading.json";
 import Lottie from "lottie-react";
+import FooTer from "../Components/Footer/Footer";
 
 const Root = () => {
   const navigation = useNavigation();
   return (
-    <div className="max-w-screen-2xl mx-auto px-3 mt-5 mb-16">
-      <NavBar />
-      {navigation.state === "loading" ? (
-        <div className=" flex justify-center items-center text-5xl md:text-9xl py-44 font-bold">
-          L <Lottie animationData={loading} /> ADING
-        </div>
-      ) : (
-        <Outlet />
-      )}
+    <div>
+      <div className="max-w-screen-2xl mx-auto px-3 mt-5 mb-16">
+        <NavBar />
+        {navigation.state === "loading" ? (
+          <div className=" flex justify-center items-center text-5xl md:text-9xl py-44 font-bold">
+            L <Lottie animationData={loading} /> ADING
+          </div>
+        ) : (
+          <Outlet />
+        )}
+      </div>
+      <FooTer />
     </div>
   );
 };
