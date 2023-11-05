f73f164150118627ff4ce6f4a1a42f857baef560
Ashfakul Islam <r4pido3@gmail.com>
2023-11-05 16:19:13 +0600

    featured foods showing on home page from database

--- src/Components/FeaturedCard/Fcards.jsx
+++ src/Components/FeaturedCard/Fcards.jsx
@@ -1,38 +1,55 @@
 import { Card, Button } from "flowbite-react";
+import { Link } from "react-router-dom";
 
-const Fcards = () => {
+const Fcards = ({ food }) => {
+  const {
+    _id,
+    food_name,
+    food_image,
+    food_quantity,
+    food_location,
+    food_expire,
+    food_note,
+    donator_name,
+    donator_image,
+  } = food;
   return (
     <Card className="rounded-xl hover:shadow-2xl duration-300 hover:-rotate-1 hover:border-green-300 ">
       <div className="space-y-3">
         <div className="flex justify-center items-center">
-          <img
-            className="w-60"
-            src="https://i.ibb.co/0Q5yKgL/apple-watch-9.png"
-          />
+          <img className="rounded-xl w-96" src={food_image} />
         </div>
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Kacchi Biriani
+        <h5 className="text-2xl ml-5 font-bold tracking-tight text-gray-900 dark:text-white">
+          {food_name}
         </h5>
-        <div>
-          <h1 className="font-medium text-xl"><span className="font-bold">Food Quantity:</span> 40 Person</h1>
-          <h1 className="font-medium text-xl"><span className="font-bold">Pickup Location:</span> Dhaka</h1>
-          <h1 className="font-medium text-xl"><span className="font-bold">Expired data:</span> 15-12-23</h1>
+        <div className="ml-5">
+          <h1 className="font-medium text-xl">
+            <span className="font-bold">Food Quantity:</span> {food_quantity}{" "}
+            Person
+          </h1>
+          <h1 className="font-medium text-xl">
+            <span className="font-bold">Pickup Location:</span> {food_location}
+          </h1>
+          <h1 className="font-medium text-xl">
+            <span className="font-bold">Expired data:</span> {food_expire}
+          </h1>
           <p>
-            <span className="font-bold">Note:</span> Hello from under the
-            water.Hello from under the water.Hello from under the water.
+            <span className="font-bold">Note:</span> {food_note}
           </p>
         </div>
         <div className="flex  justify-between items-center">
           <div className="flex items-center gap-4 my-2">
             <img
-              src="https://i.ibb.co/cczNjfw/Allura-Avatar-1.png"
+              src={donator_image}
               className="w-10 rounded-full border h-10 object-contain"
             />
-            <h1 className="font-bold text-xl">Shanto</h1>
+            <h1 className="font-bold text-xl">{donator_name}</h1>
           </div>
-          <Button className="h-10" gradientDuoTone="greenToBlue">
-            Details
-          </Button>
+          <Link to={`/food/${_id}`}>
+            <Button className="h-10" gradientDuoTone="greenToBlue">
+              Details
+            </Button>
+          </Link>
         </div>
       </div>
     </Card>
--- src/Components/FeaturedCard/FeaturedCard.jsx
+++ src/Components/FeaturedCard/FeaturedCard.jsx
@@ -1,18 +1,24 @@
 import { Button } from "flowbite-react";
 import Fcards from "./Fcards";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const FeaturedCard = () => {
+const [featured, setFeatured] = useState([])
+  useEffect(() =>{
+    axios.get('http://localhost:5000/api/availableFoods')
+    .then(res => setFeatured(res.data))
+  },[])
   return (
     <div className="space-y-8">
       <h1 className="text-center text-3xl md:text-5xl font-bold">Featured Foods</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        <Fcards />
-        <Fcards />
-        <Fcards />
-        <Fcards />
-        <Fcards />
-        <Fcards />
+        {featured.slice(0,6).map(food => {
+          return(
+            <Fcards key={food._id} food={food}/>
+          )
+        })}
       </div>
       <div className="flex justify-center items-center">
         <Link to={"/availableFoods"}>
