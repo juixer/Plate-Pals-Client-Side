0e1d979dd14a737876b22519cb9216bf3c4d7d98
Ashfakul Islam <r4pido3@gmail.com>
2023-11-05 16:59:18 +0600

    available foods page is now showin sort wise food

---
+++ src/Pages/AvailableFoods/AvailableFoodCard.jsx
@@ -0,0 +1,59 @@
+import { Card, Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const AvailableFoodCard = ({ food }) => {
+  const {
+    _id,
+    food_name,
+    food_image,
+    food_quantity,
+    food_location,
+    food_expire,
+    food_note,
+    donator_name,
+    donator_image,
+  } = food;
+  return (
+    <Card className="rounded-xl hover:shadow-2xl duration-300 hover:-rotate-1 hover:border-green-300 ">
+      <div className="space-y-3">
+        <div className="flex justify-center items-center">
+          <img className="rounded-xl w-96" src={food_image} />
+        </div>
+        <h5 className="text-2xl ml-5 font-bold tracking-tight text-gray-900 dark:text-white">
+          {food_name}
+        </h5>
+        <div className="ml-5">
+          <h1 className="font-medium text-xl">
+            <span className="font-bold">Food Quantity:</span> {food_quantity}{" "}
+            Person
+          </h1>
+          <h1 className="font-medium text-xl">
+            <span className="font-bold">Pickup Location:</span> {food_location}
+          </h1>
+          <h1 className="font-medium text-xl">
+            <span className="font-bold">Expired data:</span> {food_expire}
+          </h1>
+          <p>
+            <span className="font-bold">Note:</span> {food_note}
+          </p>
+        </div>
+        <div className="flex  justify-between items-center">
+          <div className="flex items-center gap-4 my-2">
+            <img
+              src={donator_image}
+              className="w-10 rounded-full border h-10 object-contain"
+            />
+            <h1 className="font-bold text-xl">{donator_name}</h1>
+          </div>
+          <Link to={`/food/${_id}`}>
+            <Button className="h-10" gradientDuoTone="greenToBlue">
+              Details
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default AvailableFoodCard;
--- src/Pages/AvailableFoods/AvailableFoods.jsx
+++ src/Pages/AvailableFoods/AvailableFoods.jsx
@@ -1,21 +1,34 @@
+import axios from "axios";
 import { Button, Select } from "flowbite-react";
+import { useEffect, useState } from "react";
+import AvailableFoodCard from "./AvailableFoodCard";
 
 const AvailableFoods = () => {
+  const [foods, setFoods] = useState([]);
 
   const handleExpireFood = (e) => {
-
     const selectedValue = e.target.value;
 
     if (selectedValue === "short") {
-
-      console.log("Sorting by Soonest to Expire");
+      axios
+        .get("http://localhost:5000/api/foodsExpireDataShort")
+        .then((res) => setFoods(res.data));
     } else if (selectedValue === "long") {
-        
-      console.log("Sorting by Latest to Expire");
-    }else{
-        console.log('default');
+      axios
+        .get("http://localhost:5000/api/foodsExpireDataLong")
+        .then((res) => setFoods(res.data));
+    } else {
+      axios
+        .get("http://localhost:5000/api/availableFoods")
+        .then((res) => setFoods(res.data));
     }
   };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/availableFoods")
+      .then((res) => setFoods(res.data));
+  }, []);
   return (
     <div className="my-16">
       <div className="max-w-xl mx-auto ">
@@ -35,12 +48,22 @@ const AvailableFoods = () => {
         <form className="flex justify-end">
           <div className="max-w-md">
             <Select onChange={handleExpireFood} name="expire">
-              <option value="">Sort Food</option>
+              <option value="">Default Sorted Food</option>
               <option value="short">Soonest to Expire</option>
               <option value="long">Latest to Expire</option>
             </Select>
           </div>
         </form>
+      </div>
+      <div>
+        <h1 className="text-center text-3xl md:text-5xl font-bold">
+          Food Available: {foods.length}
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-10 mt-10">
+          {foods.map((food) => {
+            return <AvailableFoodCard key={food._id} food={food} />;
+          })}
+        </div>
       </div>
     </div>
   );
